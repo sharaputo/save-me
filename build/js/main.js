9105e37b9568c093f6bba30bc510366a9191f75a
Fedor Sharaputo <sharaputo@icloud.com>
2021-11-06 19:53:11 +0300

    about block is ready

--- build/js/main.js
+++ build/js/main.js
@@ -30,5 +30,34 @@ document.addEventListener('DOMContentLoaded', function () {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
     }
-  });
+  }); // Embed youtube videos the right way
+
+  function embedYoutube() {
+    var youtubeContainers = document.querySelectorAll('.embed-youtube');
+
+    var _loop = function _loop(i) {
+      var container = youtubeContainers[i];
+      var imageSource = 'https://img.youtube.com/vi/' + container.dataset.videoId + '/sddefault.jpg';
+      var image = new Image();
+      image.src = imageSource;
+      image.addEventListener('load', function () {
+        container.appendChild(image);
+      });
+      container.addEventListener('click', function () {
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('allowfullscreen', '');
+        iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+        iframe.setAttribute('src', 'https://www.youtube.com/embed/' + this.dataset.videoId + '?rel=0&showinfo=0&autoplay=1');
+        this.innerHTML = '';
+        this.appendChild(iframe);
+      });
+    };
+
+    for (var i = 0; i < youtubeContainers.length; i++) {
+      _loop(i);
+    }
+  }
+
+  embedYoutube();
 });
